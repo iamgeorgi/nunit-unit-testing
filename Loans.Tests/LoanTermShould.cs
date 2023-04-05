40d98e031f4cee01ec74bf7457626e5b0b650168
Georgi.Petrov <georgi.petrov@entaincorp.com>
2023-04-05 11:57:34 +0300

    Add project files.

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,20 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    internal class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+    }
+}
